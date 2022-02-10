958831ab3df22268067473065c9c88c2e58a1cf2
Renan Hilario <rvahilario@gmail.com>
2022-02-10 00:33:34 -0300

    chore: change structure folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import theme from '../styles/themes/theme';
+import theme from '../themes/theme';
 import GlobalStyle from '../styles/global';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
