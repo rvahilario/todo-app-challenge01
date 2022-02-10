1d210469987029f6a2ab14a596acec36b3a4ed3d
Renan Hilario <rvahilario@gmail.com>
2022-02-10 01:06:26 -0300

    refactor: header comp

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
-import { Container } from './styles'
-import { HeaderProps } from './types'
+import { Container } from './styles';
+import { HeaderProps } from './types';
 
-export const Header = ({}: HeaderProps) => {
-  return (
-    <Container>
-      <h1>Header</h1>
-    </Container>
-  )
-}
+export const Header = ({ title }: HeaderProps) => {
+	return (
+		<Container>
+			<h1>{title}</h1>
+		</Container>
+	);
+};
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,6 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Container = styled.div`
-
-
-`
+	text-align: center;
+	border-bottom: 2px solid ${props => props.theme.colors.primary};
+`;
