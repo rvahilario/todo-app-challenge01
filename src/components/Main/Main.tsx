eafb8e58ade7cf56bed3eb2d1f735a94fec018d8
Renan Hilario <rvahilario@gmail.com>
2022-02-10 19:30:45 -0300

    feat(main): addTask comp in Main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,6 @@
 import { Header } from 'components/Header';
 import { TaskDisplay } from 'components/TaskDisplay';
+import { AddTask } from 'components/AddTask';
 import { useState } from 'react';
 import { Container, Area } from './styles';
 
@@ -11,10 +12,21 @@ export const Main = () => {
 		{ id: 2, name: 'Study tests with Jest', done: false },
 	]);
 
+	const handleAddTask = (taskName: string) => {
+		const newTask = {
+			id: list.length + 1,
+			name: taskName,
+			done: false,
+		};
+
+		setList([newTask, ...list]);
+	};
+
 	return (
 		<Container>
 			<Area>
 				<Header title='To-do list' />
+				<AddTask onEnter={handleAddTask} />
 				{list.map(task => (
 					<TaskDisplay task={task} key={task.id} />
 				))}
