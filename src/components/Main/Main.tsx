e0ab6870dd3a35b289d389b0a550153b07023fde
Renan Hilario <rvahilario@gmail.com>
2022-02-10 02:45:36 -0300

    feat(main): add taskDisplay to main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,23 @@
 import { Header } from 'components/Header';
+import { TaskDisplay } from 'components/TaskDisplay';
+import { useState } from 'react';
 import { Container, Area } from './styles';
 
+import { Task } from '../../types/task';
+
 export const Main = () => {
+	const [list, setList] = useState<Task[]>([
+		{ id: 1, name: 'Study Typescript', done: false },
+		{ id: 2, name: 'Study tests with Jest', done: false },
+	]);
+
 	return (
 		<Container>
 			<Area>
-				<Header title='Tasks List' />
+				<Header title='To-do list' />
+				{list.map(task => (
+					<TaskDisplay task={task} key={task.id} />
+				))}
 			</Area>
 		</Container>
 	);
