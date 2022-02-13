0857f8010c559c296b880b608b50405635760b66
Renan Hilario <rvahilario@gmail.com>
2022-02-13 12:12:16 -0300

    refactor(test): jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	testEnvironment: 'jsdom',
 	testPathIgnorePatterns: ['/node_modules', '/.next/'],
 	collectCoverage: true,
-	collectCoverageFrom: ['src/**/*.ts(x)?', '!stories/*.stories.tsx'],
+	collectCoverageFrom: ['src/**/*.ts(x)?'],
 	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 	testMatch: ['**/__tests__/**/?(*.)+(spec|test).[tj]s?(x)'],
 };
