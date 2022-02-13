398e03b5a34594780f81aa5f643cf948481a8e21
Renan Hilario <rvahilario@gmail.com>
2022-02-13 10:49:20 -0300

    test(header): add tests

--- __tests__/components/Header/Header.test.tsx
+++ __tests__/components/Header/Header.test.tsx
@@ -1,12 +1,16 @@
-import { render, screen } from '@testing-library/react'
-import { Header } from '../../../src/components/Header'
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { Header } from '../../../src/components/Header';
+import theme from '../../../src/themes/theme';
 
 describe('<Header />', () => {
-  it('should render Header', () => {
-    render(
-      <Header />
-    )
+	it('should render Header', () => {
+		render(
+			<ThemeProvider theme={theme}>
+				<Header title='Successful Test!' />
+			</ThemeProvider>
+		);
 
-    expect(screen.getByText('Header')).toBeInTheDocument()
-  })
-})
+		expect(screen.getByText('Successful Test!')).toBeInTheDocument();
+	});
+});
